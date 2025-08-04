f97ef539dc02df7b5b0a0eda218a2b574552442e
fortrainees2020 <64248056+fortrainees2020@users.noreply.github.com>
2025-08-04 22:23:35 +0530

    Update SpringBootProductBaseProjectIntellijApplicationTests.java

--- SpringBoot-Product-Base-Project-github-docker/src/test/java/com/example/demo/SpringBootProductBaseProjectIntellijApplicationTests.java
+++ SpringBoot-Product-Base-Project-github-docker/src/test/java/com/example/demo/SpringBootProductBaseProjectIntellijApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
+@SpringBootTest(properties = {"spring.main.allow-bean-definition-overriding=true"})
+
 class SpringBootProductBaseProjectIntellijApplicationTests {
 
 	@Test
